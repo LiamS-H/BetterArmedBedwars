cc479c8202c56cbad4e19d795127147ac2d0002a
Liam Stelly-Hawkes <liamsh@gmail.com>
2024-08-29 21:13:59 -0700

    fix typo

--- src/main/java/com/github/liamsh/BetterArmedBedwars/utils/ServerData.java
+++ src/main/java/com/github/liamsh/BetterArmedBedwars/utils/ServerData.java
@@ -103,7 +103,7 @@ public class ServerData {
             mode = locrawData.get("mode").getAsString();
             gameType = mode;
         } catch (JsonSyntaxException e) {
-            // json was in unexpect format (cause in lobby servers)
+            // json was in unexpected format (cause in lobby servers)
         } catch (Exception e) {
             // null error
         }
